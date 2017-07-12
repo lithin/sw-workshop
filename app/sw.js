2a22970db967bf641e84af5f25740f81a383375d
Anna Doubkova <doubkova.anna@gmail.com>
2017-07-12 16:34:12 +0100

    Simplify

--- app/sw.js
+++ app/sw.js
@@ -1,7 +1,7 @@
 const CACHE_VERSION = '3'
 
 self.addEventListener('install', function(event) {
-  console.log('Hello DevFest Berlin 2016!');
+  console.log('Hello FullStack 2017!');
   event.waitUntil(function() {
     return caches.open(CACHE_VERSION)
       .then(cache => {
@@ -16,40 +16,9 @@ self.addEventListener('install', function(event) {
 });
 self.addEventListener('fetch', function(event) {
   event.respondWith(fetch(event.request)
-    .then(response => {
-
-      if (event.request.url.endsWith('/get')) {
-        caches.open(CACHE_VERSION)
-          .then(cache => {
-            cache.put(event.request, response);
-          });
-
-        return response.clone();
-      }
-      return response;
-    })
     .catch(err => {
-      console.log(event.request)
       return caches.match(event.request)
         .then(response => {
-          // check if response is cached with cached statusText
-          if (response && response.statusText == 'OK') {
-            const init = {
-              "status" : 200,
-              "statusText" : "Cached"
-            };
-            return response.blob()
-              .then(blob => {
-                var newResponse = new Response(blob, init);
-                caches.open(CACHE_VERSION)
-                  .then(cache => {
-                    cache.put(event.request, newResponse);
-                  });
-                console.log(newResponse.clone())
-                return newResponse.clone();
-              });
-          }
-          // already modfied
           return response;
         });
     }));
